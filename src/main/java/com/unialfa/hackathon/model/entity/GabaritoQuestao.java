5aa85d952537c0d9b6de295cd62401e8edb777e8
Ryan1590 <79643430+Ryan1590@users.noreply.github.com>
2025-06-19 13:05:08 -0300

    Update GabaritoQuestao.java
    GabaritoQuestao

---
+++ src/main/java/com/unialfa/hackathon/model/entity/GabaritoQuestao.java
@@ -0,0 +1,15 @@
+package com.unialfa.hackathon.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GabaritoQuestao {
+    private String resposta;
+    private double valor;
+}
