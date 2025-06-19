f6a82c2b7daabddbb41667dbd1758951e7c136ba
Ryan1590 <79643430+Ryan1590@users.noreply.github.com>
2025-06-19 13:04:49 -0300

    Update Gabarito.java
    Gabarito

---
+++ src/main/java/com/unialfa/hackathon/model/entity/Gabarito.java
@@ -0,0 +1,33 @@
+package com.unialfa.hackathon.model.entity;
+
+import com.unialfa.hackathon.model.converter.GabaritoQuestaoConverter;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.HashMap; // <-- IMPORTAR
+import java.util.Map;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Gabarito implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "prova_id", nullable = false, unique = true)
+    private Prova prova;
+
+    //
+    // AQUI ESTÁ A CORREÇÃO:
+    // Inicializamos o mapa para que ele nunca seja nulo.
+    //
+    @Convert(converter = GabaritoQuestaoConverter.class)
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private Map<Integer, GabaritoQuestao> respostas = new HashMap<>();
+}
